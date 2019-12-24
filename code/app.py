d529740b84c1e88bbfa16dbdf5d9bc3d06774ac6
franmorfe <franmorfe@gmail.com>
2019-12-24 14:30:16 +0100

    Implemented Flask-SQLAlchemy

---
+++ code/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+from flask_restful import Api
+from flask_jwt import JWT
+from datetime import timedelta
+from db import db
+
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+
+app = Flask(__name__)
+
+# App Configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_AUTH_URL_RULE'] = '/login'
+app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=3600)
+app.secret_key = 'Your-best-secret'
+api = Api(app)
+db.init_app(app)
+
+# Tables DB creation
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+jwt = JWT(app, authenticate, identity)
+
+# Endpoints
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
